f768677db5f6ed64b153b30ec9bb27322ec1c39e
Diana B <ada_wit60@hackademy.mx>
2022-06-15 14:14:29 -0500

    Adding icons in Tab.Navigator

--- App.js
+++ App.js
@@ -9,9 +9,21 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Feed") {
+              iconName = focused ? "book" : "book-outline";
+            } else if (route.name === "Crear") {
+              iconName = focused ? "create" : "create-outline";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
         <Tab.Screen name="Feed" component={Feed} />
-        <Tab.Screen name="Crear Historia" component={CreateStory} />
+        <Tab.Screen name="Crear" component={CreateStory} />
       </Tab.Navigator>
     </NavigationContainer>
   );
