ed5df064548782478c4c3d4f0dcde7d31e2e5d4f
Diana B <ada_wit60@hackademy.mx>
2022-06-15 18:39:55 -0500

    Implementing Drawer Navigator y Tab Navigator to show new windows

--- App.js
+++ App.js
@@ -1,39 +1,11 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import Feed from "./screens/Feed";
-import CreateStory from "./screens/CreateStory";
-
-const Tab = createBottomTabNavigator();
+import DrawerNavigator from "./navigation/DrawerNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name === "Feed") {
-              iconName = focused ? "book" : "book-outline";
-            } else if (route.name === "Crear") {
-              iconName = focused ? "create" : "create-outline";
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-
-          tabBarActiveTintColor: "tomato",
-          tabBarInactiveTintColor: "gray",
-          tabBarStyle: [
-            {
-              display: "flex",
-            },
-            null,
-          ],
-        })}
-      >
-        <Tab.Screen name="Feed" component={Feed} />
-        <Tab.Screen name="Crear" component={CreateStory} />
-      </Tab.Navigator>
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
