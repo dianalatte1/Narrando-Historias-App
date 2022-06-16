efdaf60ebe228ad702a66b0aa54d2ed51b1f4391
Diana B <ada_wit60@hackademy.mx>
2022-06-15 20:18:02 -0500

    Showing storyCard with FlatList

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StyleSheet } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import Feed from "../screens/Feed";
 import CreateStory from "../screens/CreateStory";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { RFValue } from "react-native-responsive-fontsize";
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator
+      labeled={false}
+      barStyle={styles.bottomTabStyle}
       screenOptions={({ route }) => ({
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
@@ -17,23 +23,47 @@ const BottomTabNavigator = () => {
           } else if (route.name === "Crear") {
             iconName = focused ? "create" : "create-outline";
           }
-          return <Ionicons name={iconName} size={size} color={color} />;
+          return (
+            <Ionicons
+              name={iconName}
+              size={RFValue(25)}
+              color={color}
+              style={styles.icons}
+            />
+          );
         },
 
-        tabBarActiveTintColor: "tomato",
-        tabBarInactiveTintColor: "gray",
-        tabBarStyle: [
-          {
-            display: "flex",
-          },
-          null,
-        ],
+        // tabBarActiveTintColor: "tomato",
+        // tabBarInactiveTintColor: "gray",
+        // tabBarStyle: [
+        //   {
+        //     display: "flex",
+        //   },
+        //   null,
+        // ],
       })}
+      activeColor={"#ee8249"}
+      inactiveColor={"gray"}
     >
       <Tab.Screen name="Feed" component={Feed} />
       <Tab.Screen name="Crear" component={CreateStory} />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: "#2f345d",
+    height: "8%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    overflow: "hidden",
+    position: "absolute",
+  },
+  icons: {
+    width: RFValue(30),
+    height: RFValue(30),
+  },
+});
 
 export default BottomTabNavigator;
--- screens/Feed.js
+++ screens/Feed.js
@@ -1,12 +1,109 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  Image,
+} from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { FlatList } from "react-native-gesture-handler";
+import StoryCard from "./StoryCard";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf"),
+};
+
+let stories = require("./temp.json");
 
 export default class Feed extends Component {
+  state = {
+    fontsLoaded: false,
+  };
+
+  async loadFonts() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+  componentDidMount() {
+    this.loadFonts();
+  }
+
+  renderItem = ({ item: story }) => {
+    return <StoryCard story={story} />;
+  };
+
+  keyExtractor = (item, index) => index.toString();
   render() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Indice</Text>
-      </View>
-    );
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>
+                Aplicación para narrar Historias
+              </Text>
+            </View>
+          </View>
+          <View style={styles.cardContainer}>
+            <FlatList
+              keyExtractor={this.keyExtractor}
+              data={stories}
+              renderItem={this.renderItem}
+            />
+          </View>
+        </View>
+      );
+    }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c",
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    padding: 5,
+  },
+  appIcon: {
+    flex: 0.3,
+  },
+  appTitleTextContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: 28,
+    fontFamily: "Bubblegum-Sans",
+    paddingLeft: 20,
+  },
+  cardContainer: {
+    flex: 0.85,
+  },
+  iconImage: {
+    width: 60,
+    height: 60,
+    resizeMode: "contain",
+    marginLeft: 10,
+  },
+});
